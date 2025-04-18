be0153c31b7f2ac33f72cd4cacb59640b0189c85
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-04-18 05:40:52 +0300

    column styles updated

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -8,11 +8,22 @@ interface ColumnProps {
 const ColumnTitle = styled.h2`
   margin: 0;
   display: inline-block;
+  text-align: center;
+`
+const ColumnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 200px;
+  background-color: #f2f2f2;
+  padding: 1rem;
+  border-radius: 0.5rem;
 `
 
 const Column = ({ status }: ColumnProps) => {
   return (
-    <ColumnTitle>{status}</ColumnTitle>
+    <ColumnWrapper>
+      <ColumnTitle>{status}</ColumnTitle>
+    </ColumnWrapper>
   )
 }
 
