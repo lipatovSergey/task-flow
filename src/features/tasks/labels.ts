4f005d7d005ba02682801c3ac75b899d7b61a554
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-04-19 06:09:25 +0300

    labels for task's priority and status added

---
+++ src/features/tasks/labels.ts
@@ -0,0 +1,11 @@
+export const statusLabels = {
+  ToDO: 'To Do',
+  InProgress: 'In Progress',
+  Done: 'Done'
+}
+
+export const priorityLabel = {
+  1: 'High',
+  2: 'Regular',
+  3: 'Low'
+}
--- src/features/tasks/mockData.ts
+++ src/features/tasks/mockData.ts
@@ -7,7 +7,7 @@ export const mockTasks: Task[] = [
     description: "Create folders, add base files, and clean up unnecessary ones",
     status: "Done",
     createdAt: new Date("2024-04-01"),
-    priority: "regular",
+    priority: 2,
   },
   {
     id: '2',
@@ -15,7 +15,7 @@ export const mockTasks: Task[] = [
     description: "Create base wrapper and styling using styled-components",
     status: "Done",
     createdAt: new Date("2024-04-02"),
-    priority: "low",
+    priority: 3,
   },
   {
     id: '3',
@@ -23,7 +23,7 @@ export const mockTasks: Task[] = [
     description: "Add the library and apply it in the project",
     status: "InProgress",
     createdAt: new Date("2024-04-03"),
-    priority: "regular",
+    priority: 2,
   },
   {
     id: '4',
@@ -31,7 +31,7 @@ export const mockTasks: Task[] = [
     description: "Filter and display tasks in the correct column",
     status: "ToDo",
     createdAt: new Date("2024-04-04"),
-    priority: "high",
+    priority: 1,
   },
   {
     id: "5",
@@ -39,7 +39,7 @@ export const mockTasks: Task[] = [
     description: "Design and implement a component to display a single task",
     status: "ToDo",
     createdAt: new Date("2024-04-05"),
-    priority: "regular",
+    priority: 2,
   },
   {
     id: '6',
@@ -47,7 +47,7 @@ export const mockTasks: Task[] = [
     description: "Set up a library and test task movement",
     status: "InProgress",
     createdAt: new Date("2024-04-06"),
-    priority: "high",
+    priority: 1,
   },
 ];
 
