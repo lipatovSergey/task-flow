62f279cfa9d13f6f43570468dcc7fc755d3cee95
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-04-25 07:52:15 +0300

    Created seperate file with styled component for modal forms

---
+++ src/components/FormStyles.ts
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const BackDrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+`;
+
+export const TaskFormModal = styled.div`
+  position: relative;
+  padding: 8px;
+  background-color: white;
+  border-radius: 8px;
+  max-width: 500px;
+  min-height: 250px;
+  width: 90%;
+  z-index: 1000;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  .fields {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .fields :is(input, select):not(:last-child) {
+    margin-bottom: 8px;
+  }
+
+  button {
+    margin-top: 16px;
+  }
+`;
+
+export const CloseModalBtn = styled.button`
+  position: absolute;
+  right: -10px;
+  top: -10px;
+`;
+
+export const ErrorSpan = styled.span`
+  color: #e60000;
+  font-size: 12px;
+`;
--- src/components/TaskAddForm.tsx
+++ src/components/TaskAddForm.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import type { Task } from "../features/tasks/tasksTypes";
+import {
+  BackDrop,
+  TaskFormModal,
+  Form,
+  ErrorSpan,
+  CloseModalBtn,
+} from "./FormStyles";
 
 interface TaskAddFormProps {
   isOpen: boolean;
@@ -8,56 +14,6 @@ interface TaskAddFormProps {
   onTaskAdd: (task: Task) => void;
 }
 
-const BackDrop = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgb(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-`;
-
-const TaskFormModal = styled.div`
-  position: relative;
-  padding: 8px;
-  background-color: white;
-  border-radius: 8px;
-  max-width: 500px;
-  min-height: 250px;
-  width: 90%;
-  z-index: 1000;
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-
-  .fields {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .fields :is(input, select):not(:last-child) {
-    margin-bottom: 8px;
-  }
-
-  button {
-    margin-top: 16px;
-  }
-`;
-const CloseModalBtn = styled.button`
-  position: absolute;
-  right: -10px;
-  top: -10px;
-`;
-const ErrorSpan = styled.span`
-  color: #e60000;
-  font-size: 12px;
-`;
 const TaskAddForm = ({ setIsOpen, onTaskAdd }: TaskAddFormProps) => {
   // state for errors in form fields
   const [errors, setErrors] = useState({
