8af234acc20c7ef89cc3b200b4db456b20902665
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-04-25 11:54:21 +0300

    task types in backend

---
+++ backend/src/models/task.ts
@@ -0,0 +1,10 @@
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: "ToDo" | "InProgress" | "Done";
+  createdAt: Date;
+  priority: 1 | 2 | 3;
+}
+
+export let tasks: Task[] = [];
