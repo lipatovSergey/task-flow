6e0a54ebeaec833d5ccd3c3d594eb77037c93f1d
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-04-26 09:31:02 +0300

    cors added to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express from "express";
 import taskRoutes from "./routes/taskRoutes";
 import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/tasks", taskRoutes);
 app.use(errorHandler);
